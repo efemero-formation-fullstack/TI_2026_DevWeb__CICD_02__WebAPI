5e24692187b5ea6cb9f78b076b34d13a2b033ae8
François Bastien <fmrbastien@gmail.com>
2026-05-07 13:39:51 +0200

    init the project, make it runnable

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import calcService from './services/calc.service';
+import express from "express";
+import calcService from "./services/calc.service.js";
 
 // Env
 const { PORT } = process.env;
@@ -8,35 +8,35 @@ const { PORT } = process.env;
 const app = express();
 
 // Routes
-app.get('/api/hello', (req, res) => {
-  res.json({ message : '👽 Bonjour le monde 👽' })
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "👽 Bonjour le monde 👽" });
 });
 
-app.get('/api/even/:nb', (req, res) => {
+app.get("/api/even/:nb", (req, res) => {
   const nb = parseInt(req.params.nb);
   const isEven = calcService.isEven(nb);
 
   res.json({
-    message: `La valeur ${nb} est ${isEven ? 'paire' : 'impaire'}`
+    message: `La valeur ${nb} est ${isEven ? "paire" : "impaire"}`,
   });
 });
 
-app.get('/api/addition', (req, res) => {
+app.get("/api/addition", (req, res) => {
   const nb1 = parseFloat(req.query.nb1);
   const nb2 = parseFloat(req.query.nb2);
 
-  if(isNaN(nb1) || isNaN(nb2)) {
-    res.statusCode(422).json({
-      message: 'Boulet ! Donne des parametres nb1 et nb2 valides 😡'
+  if (isNaN(nb1) || isNaN(nb2)) {
+    res.status(422).json({
+      message: "Boulet ! Donne des parametres nb1 et nb2 valides 😡",
     });
     return;
   }
 
   const result = calcService.add(nb1, nb2);
   res.json({
-    message: `Le resultat de l'addition de ${nb1} et ${nb2} est ${result}`
+    message: `Le resultat de l'addition de ${nb1} et ${nb2} est ${result}`,
   });
-})
+});
 
 // Start app
 app.listen(PORT, () => {
